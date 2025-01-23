b9b2a522eda40db4157044da6e2f4e4a365289a5
Yashik <147544198+bytebeaver@users.noreply.github.com>
2025-01-23 23:52:27 +0530

    Time: 0 ms (100%), Space: 10.1 MB (24.4%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.cpp
@@ -8,32 +8,54 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ 
+
+ //BRUTE FORCE:
+// class Solution {
+// public:
+//     ListNode* middleNode(ListNode* head) {
+        
+//         ListNode *temp = head;
+//         int count=0;
+
+//         while(temp != NULL)
+//         {
+//             count++;
+//             temp=temp->next;
+//         }
+
+//         int middleNode = count/2 + 1;
+
+//         temp = head;
+
+//         while(temp != NULL)
+//         {
+//             middleNode = middleNode-1;
+
+//             if(middleNode == 0)
+//             break;
+
+//             temp = temp->next;
+//         }
+//         return temp;
+//     }
+// };
+
+//OPTIMIZED : USING TORTOISE HARE ALGORITHM (STRIVER)
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
         
-        ListNode *temp = head;
-        int count=0;
+        ListNode *slow = head;
+        ListNode *fast = head;
 
-        while(temp != NULL)
+        while(fast != NULL /*in case n is even*/  && fast->next != NULL /*n is odd*/) //n = number of nodes in linked list
         {
-            count++;
-            temp=temp->next;
+            slow = slow->next;
+            fast = fast->next->next;
+
         }
 
-        int middleNode = count/2 + 1;
-
-        temp = head;
-
-        while(temp != NULL)
-        {
-            middleNode = middleNode-1;
-
-            if(middleNode == 0)
-            break;
-
-            temp = temp->next;
-        }
-        return temp;
+        return slow;
     }
 };
