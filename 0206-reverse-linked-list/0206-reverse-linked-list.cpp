9e73e54b6e289c2a52ae1cb527064a09008e71a0
Yashik <147544198+bytebeaver@users.noreply.github.com>
2025-01-24 01:14:13 +0530

    Time: 0 ms (100%), Space: 13.6 MB (10.43%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -8,22 +8,44 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+ //ITERATIVE : 
+
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode *temp = head;
+//         ListNode *back = NULL;
+
+//         ListNode *front;
+
+//         while(temp != NULL)
+//         {
+//             front = temp->next;
+//             temp->next = back;
+//             back = temp;
+//             temp = front;
+//         }
+
+//     return back;
+//     }
+// };
+
+
+//RECURSIVE:
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode *temp = head;
-        ListNode *back = NULL;
+        
+        if(head == NULL || head->next == NULL)
+        return head;
 
-        ListNode *front;
+        ListNode *NewHead = reverseList(head->next);
+        ListNode *front = head->next;
+        front->next = head;
+        head->next=NULL;
 
-        while(temp != NULL)
-        {
-            front = temp->next;
-            temp->next = back;
-            back = temp;
-            temp = front;
-        }
-
-    return back;
+        return NewHead;
     }
 };
+
