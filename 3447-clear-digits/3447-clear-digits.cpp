3d276759ed606c880f0d9715c525402f1809ec6d
Yashik <147544198+bytebeaver@users.noreply.github.com>
2025-02-11 01:25:24 +0530

    Time: 0 ms (100%), Space: 9.1 MB (41.58%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,25 +1,64 @@
+//Code story with MIK
+
+// class Solution {
+// public:
+
+    // approach 1 brute force  ---- T.C -> O(N^2)
+
+//     string clearDigits(string s) {
+        
+//         int i=0;
+//         while(i<s.length() )
+//         {
+//             if( isdigit( s[i] ) )
+//             {
+//                 s.erase(i,1);
+//                 if(i-1>=0)
+//                 {
+//                     s.erase(i-1,1);
+//                     i--;
+//                 }
+//             }
+//             else
+//             i++;
+//         }
+//         return s;
+//     }
+// };
+
+
 class Solution {
 public:
 
-    //brute force
+    //Approach 2 ---> using stack 
+                    //S.C -> O(N)
+                    //T.C -> O(N)
 
     string clearDigits(string s) {
         
-        int i=0;
-        while(i<s.length() )
+        stack <char> st;
+
+        for(char &ch : s)
         {
-            if( isdigit( s[i] ) )
+            if( isdigit( ch ) )
             {
-                s.erase(i,1);
-                if(i-1>=0)
+                if( !st.empty() )
                 {
-                    s.erase(i-1,1);
-                    i--;
+                    st.pop();
                 }
             }
             else
-            i++;
+            st.push( ch );
         }
-        return s;
+
+        string result = "";
+        while(!st.empty() )
+        {
+            result.push_back( st.top() );
+            st.pop();
+        }
+
+        reverse(begin(result), end(result));
+        return result;
     }
 };
