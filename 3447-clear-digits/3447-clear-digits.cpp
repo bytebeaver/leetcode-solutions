04fa3f19f77bed7b6020329e2b76be84859d3078
Yashik <147544198+bytebeaver@users.noreply.github.com>
2025-02-11 01:17:03 +0530

    Time: 0 ms (100%), Space: 8.2 MB (96.32%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    //brute force
+
+    string clearDigits(string s) {
+        
+        int i=0;
+        while(i<s.length() )
+        {
+            if( isdigit( s[i] ) )
+            {
+                s.erase(i,1);
+                if(i-1>=0)
+                {
+                    s.erase(i-1,1);
+                    i--;
+                }
+            }
+            else
+            i++;
+        }
+        return s;
+    }
+};
