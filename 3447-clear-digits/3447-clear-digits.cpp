2bae2b0223417454d218c6bc442c29b2dd44f10a
Yashik <147544198+bytebeaver@users.noreply.github.com>
2025-02-11 01:41:03 +0530

    Time: 0 ms (100%), Space: 8.4 MB (70.3%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -27,38 +27,110 @@
 // };
 
 
-class Solution {
-public:
+// class Solution {
+// public:
 
     //Approach 2 ---> using stack 
                     //S.C -> O(N)
                     //T.C -> O(N)
 
+//     string clearDigits(string s) {
+        
+//         stack <char> st;
+
+//         for(char &ch : s)
+//         {
+//             if( isdigit( ch ) )
+//             {
+//                 if( !st.empty() )
+//                 {
+//                     st.pop();
+//                 }
+//             }
+//             else
+//             st.push( ch );
+//         }
+
+//         string result = "";
+//         while(!st.empty() )
+//         {
+//             result.push_back( st.top() );
+//             st.pop();
+//         }
+
+//         reverse(begin(result), end(result));
+//         return result;
+//     }
+// };
+
+
+
+// class Solution {
+// public:
+
+    //Approach 3---> using another empty string
+                    //S.C -> O(N)
+                    //T.C -> O(N)
+
+// string clearDigits(string s) {
+        
+//         string result="";
+
+//         for(char &ch : s)
+//         {
+//             if( isdigit( ch ) )
+//             {
+//                if(!result.empty())
+//                {
+//                 result.pop_back();
+
+//                }
+//             }
+//             else
+//             result.push_back( ch );
+//         }
+
+        
+//         return result;
+//     }
+// };
+
+
+
+class Solution {
+public:
+
+    //Approach 4--->  
+                    //S.C -> O(1)
+                    //T.C -> O(N)
+
     string clearDigits(string s) {
         
-        stack <char> st;
+        int n = s.length();
 
-        for(char &ch : s)
+        int i=0,j=0;
+
+        while(i<n)
         {
-            if( isdigit( ch ) )
-            {
-                if( !st.empty() )
-                {
-                    st.pop();
-                }
+            if( isdigit( s[i] ) )
+            {   
+                j=max(j-1, 0); // to prevent j from going negative
+                // j--;//in future if i finds a valid character we will put it at jth index
             }
+
             else
-            st.push( ch );
+            {
+                s[j]=s[i];
+                j++;
+            }
+
+            i++;
         }
 
-        string result = "";
-        while(!st.empty() )
-        {
-            result.push_back( st.top() );
-            st.pop();
-        }
+    return s.substr(0, j);
 
-        reverse(begin(result), end(result));
-        return result;
     }
 };
+
+
+
