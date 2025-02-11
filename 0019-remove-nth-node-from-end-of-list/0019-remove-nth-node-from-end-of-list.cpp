fe54e8ba8231e7cb59b158bbcdd41c091e878558
Yashik <147544198+bytebeaver@users.noreply.github.com>
2025-02-11 23:49:43 +0530

    Time: 0 ms (100%), Space: 15 MB (59.41%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -8,52 +8,98 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
+//STRIVER
+
+
+ //BRUTE FORCE ----> MY OWN SOLUTION 
+                //strivers brute force is similar ....check in notes
+// class Solution {
+// public:
+//     ListNode* removeNthFromEnd(ListNode* head, int n) {
+        
+//         if(head == NULL || head->next == NULL)
+//         return NULL;
+
+//         int length=0;
+
+//         ListNode *temp = head;
+
+//         while(temp != NULL)
+//         {
+//             length++;
+//             temp=temp->next;
+//         }
+        
+//         //nth node from back is length-n+1 th node from front
+//         int k = length-n+1;
+
+//         if(k==1)
+//         {
+//             ListNode *newhead = head->next;
+//             head->next= NULL;
+//             return newhead;
+//         }
+//         int i=1;
+
+//         ListNode *prev=head;
+//         ListNode *current=head->next;
+
+//         while(current != NULL)
+//         {
+//             if(i==k-1)
+//             {
+//                 //remove the node
+//                 prev->next = current->next;
+//                 delete current;
+//                 break;
+//             }
+
+//             i++;
+//             prev=prev->next;
+//             current=current->next;
+//         }
+        
+//     return head;
+//     }
+// };
+
+
+
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         
-        if(head == NULL || head->next == NULL)
-        return NULL;
+        //we will use fast and slow pointer but NOT like tortoise hare algorithm
 
-        int length=0;
+        ListNode *slow = head;
+        ListNode *fast = head;
 
-        ListNode *temp = head;
-
-        while(temp != NULL)
+        //we are moving the fast n steps ahead
+        for(int i=1; i<=n ; i++)
         {
-            length++;
-            temp=temp->next;
+            fast=fast->next;
         }
-        
-        //nth node from back is length-n+1 th node from front
-        int k = length-n+1;
 
-        if(k==1)
+        if(fast == NULL)//it means we have to delete the first node
         {
             ListNode *newhead = head->next;
-            head->next= NULL;
+            delete head;
             return newhead;
         }
-        int i=1;
 
-        ListNode *prev=head;
-        ListNode *current=head->next;
-
-        while(current != NULL)
+        //now we will move fast and slow one at a time till "fast" reaches the last node 
+        //by doing this "slow" will reach the node previous to the node to be deleted
+        while(fast->next != NULL)
         {
-            if(i==k-1)
-            {
-                //remove the node
-                prev->next = current->next;
-                delete current;
-                break;
-            }
-
-            i++;
-            prev=prev->next;
-            current=current->next;
+            slow=slow->next;
+            fast=fast->next;
         }
-        
-    return head;
+
+        ListNode *tobeDeleted = slow->next;
+        slow->next=slow->next->next; //slow = tobeDeleted->next
+        delete tobeDeleted;
+
+        return head;
     }
 };
