208bb558c61710fffe5604517ede8a14b4ffca55
Yashik <147544198+bytebeaver@users.noreply.github.com>
2025-02-19 15:59:24 +0530

    Time: 4 ms (63.82%), Space: 8 MB (90.21%) - LeetHub

--- 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1516-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,12 +1,72 @@
+//CODESTORY WITH MIK
+
+//BACKTRACKING
+//1. DO
+//2. TRY
+//3. EXPLORE
+
+
+// class Solution {
+// public:
+
+
+//     void solve(string &curr, vector<string> &result, int &n)
+//     {
+//         if(curr.size() == n)
+//         {
+//             result.push_back(curr);
+//             return;
+//         }
+
+//         for(char ch='a' ; ch<='c';ch++)
+//         {
+//             if(!curr.empty() && curr.back() == ch)
+//             continue;
+
+//             //DO
+//             curr.push_back(ch);
+
+//             //EXPLORE
+//             solve(curr, result, n);
+
+//             //UNDO
+//             curr.pop_back();
+            
+//         }
+//     }
+//     string getHappyString(int n, int k) {
+        
+//         string curr = "";
+
+//         vector<string> result;
+
+//         solve(curr, result, n);
+
+//         if(result.size() < k)
+//         return "";
+
+//         return result[k-1];
+
+//     }
+// };
+
+
+
+// OPTIMZED APPROACH ---- same backtracking but we dont need o store all the sequences in result
 class Solution {
 public:
 
 
-    void solve(string &curr, vector<string> &result, int &n)
+    void solve(string &curr, string &result, int &n, int &counter, int &k)
     {
         if(curr.size() == n)
         {
-            result.push_back(curr);
+            // result.push_back(curr);
+            counter++;
+            if(counter == k)
+            {
+                result = curr;
+            }
             return;
         }
 
@@ -19,7 +79,7 @@ public:
             curr.push_back(ch);
 
             //EXPLORE
-            solve(curr, result, n);
+            solve(curr, result, n, counter , k);
 
             //UNDO
             curr.pop_back();
@@ -30,14 +90,14 @@ public:
         
         string curr = "";
 
-        vector<string> result;
+        string result="";
+        int counter=0;
 
-        solve(curr, result, n);
+        solve(curr, result, n, counter, k);
 
-        if(result.size() < k)
-        return "";
+      
 
-        return result[k-1];
+        return result;
 
     }
 };
