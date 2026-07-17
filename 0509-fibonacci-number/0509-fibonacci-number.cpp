33235518df9546dd5ac958c9878ba83ebfcb6a94
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-07-17 10:06:55 +0530

    Time: 3 ms (65.74%), Space: 8.3 MB (11.18%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    int fibonacci(int n , vector<int> &dp)
+    {
+        if(n<=1) return n;
+
+        //checking if subproblem is already computer or not
+        if(dp[n]!=-1)  return dp[n];
+
+        dp[n]=fibonacci(n-1, dp) + fibonacci(n-2, dp);
+
+        return dp[n];
+    }
+    int fib(int n) {
+        vector<int> dp(n+1, -1);
+        return fibonacci(n, dp);
+    }
+};
