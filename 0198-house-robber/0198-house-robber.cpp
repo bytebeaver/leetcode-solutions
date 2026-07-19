d928caab31c75314cf9153b6eea722d5af67769a
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-07-19 14:58:23 +0530

    Time: 0 ms (100%), Space: 10.1 MB (89.88%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,3 +1,42 @@
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+        
+//         int n = nums.size();
+
+//         if(n==1)
+//         return nums[0];
+
+//         if(n==2)
+//         return max(nums[0], nums[1]);
+
+//         vector<int>dp(n,0);
+        
+//         dp[n-1] = nums[n-1];
+//         dp[n-2] = max(nums[n-1], nums[n-2]);
+
+       
+//          int i=n-3;
+
+//          while(i>=0)
+//          {
+//             int take = dp[i+2] + nums[i];
+            
+//             int not_take= 0+ dp[i+1];
+
+//             dp[i] =  max(take, not_take);
+
+//             i--;
+
+//          }
+
+//          return max(dp[0], dp[1]);
+    
+//     }
+// };
+
+
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -10,27 +49,18 @@ public:
         if(n==2)
         return max(nums[0], nums[1]);
 
-        vector<int>dp(n,0);
-        
-        dp[n-1] = nums[n-1];
-        dp[n-2] = max(nums[n-1], nums[n-2]);
+        int v1 = nums[0];
+        int v2= nums[1];
 
-       
-         int i=n-3;
+        for(int i=2; i<n; i++)
+        {
+            int take = nums[i] + v1;
+            int not_take = 0 + v2;
+            v1 = max(v1,not_take);
+            v2= max(v2,take);
+        }
 
-         while(i>=0)
-         {
-            int take = dp[i+2] + nums[i];
-            
-            int not_take= 0+ dp[i+1];
-
-            dp[i] =  max(take, not_take);
-
-            i--;
-
-         }
-
-         return max(dp[0], dp[1]);
+        return max(v1,v2);
     
     }
 };
