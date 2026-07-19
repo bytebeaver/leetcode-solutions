2e2aabb7e0b3481e82cb254f6db9f81502a243cf
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-07-19 15:48:23 +0530

    Time: 0 ms (100%), Space: 10.7 MB (23.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -35,6 +35,7 @@
 //     }
 // };
 
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -47,23 +48,58 @@ public:
         if(n==2)
         return max(nums[0], nums[1]);
 
-        int prev = nums[0];
+        vector<int>dp(n,0);
+        
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
 
-        int prev2 = 0;
+       
+         int i=2;
 
-        for(int i=1; i<n; i++)
-        {
-            int take = nums[i] + prev2;
+         while(i<n)
+         {
+            int take = dp[i-2] + nums[i];
+            
+            int not_take= 0+ dp[i-1];
 
-            int not_take = 0 + prev;
+            dp[i] =  max(take, not_take);
 
-            int current_i = max(take, not_take);
-            prev2=prev;
-            prev=current_i;
+            i++;
 
+         }
 
-        }
-
-       return prev;
+         return dp[n-1];
+    
+    
     }
 };
+
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+
+//         int n = nums.size();
+
+//         if(n==1)
+//         return nums[0];
+
+//         if(n==2)
+//         return max(nums[0], nums[1]);
+
+//         int prev  = nums[0];   // best answer using only index 0
+//         int prev2 = 0;         // best answer using NO elements (phantom base, like index -1)
+
+//         for(int i=1; i<n; i++)
+//         {
+//             int take = nums[i] + prev2;    // include nums[i] -> add to two-back answer
+//             int not_take = 0 + prev;        // exclude nums[i] -> carry forward one-back answer
+
+//             int current_i = max(take, not_take);   // best answer up to index i
+
+//             prev2 = prev;         // old "one-back" becomes new "two-back"
+//             prev = current_i;     // new answer becomes new "one-back"
+//         }
+
+//         return prev;   // after the loop, prev holds the answer for the LAST index
+//     }
+// };
