776f6dfcb489415efc3003be6dfa78d5f9f3a1d8
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-07-27 17:58:27 +0530

    Time: 0 ms (100%), Space: 27.2 MB (75.05%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.cpp
@@ -1,28 +1,64 @@
+// class Solution {
+// public:
+//     int maximumSum(vector<int>& arr) {
+        
+//         int n = arr.size();
+//         if(n==1)
+//         return arr[0];
+
+//         int no_delete = max(arr[0]+arr[1], arr[1]);
+//         int one_delete = max(arr[1], arr[0]);
+
+//         int result = max(one_delete, no_delete);
+
+//         for(int i=2; i<n; i++)
+//         {
+//             int prev_no_delete = no_delete;
+//             int prev_one_delete = one_delete;
+
+//             no_delete = max(prev_no_delete + arr[i], arr[i]);
+
+//             one_delete = max(prev_one_delete + arr[i], prev_no_delete);
+
+//             result = max(result, max(no_delete, one_delete) );
+//         }
+
+//     return result;
+//     }
+// };
+
+
 class Solution {
 public:
     int maximumSum(vector<int>& arr) {
-        
         int n = arr.size();
-        if(n==1)
-        return arr[0];
+        if (n == 1)
+            return arr[0];
 
-        int no_delete = max(arr[0]+arr[1], arr[1]);
-        int one_delete = max(arr[1], arr[0]);
+        int no_delete = arr[0];
+        int one_delete = arr[1];
 
-        int result = max(one_delete, no_delete);
+        int result = max(no_delete, one_delete);
 
-        for(int i=2; i<n; i++)
+        for (int i = 1; i < n; i++)
         {
             int prev_no_delete = no_delete;
             int prev_one_delete = one_delete;
 
+            if (i == 1)
+            {
+                one_delete = max(prev_one_delete, prev_no_delete);   // max(arr[1], arr[0])
+            }
+            else
+            {
+                one_delete = max(prev_one_delete + arr[i], prev_no_delete);
+            }
+
             no_delete = max(prev_no_delete + arr[i], arr[i]);
 
-            one_delete = max(prev_one_delete + arr[i], prev_no_delete);
-
-            result = max(result, max(no_delete, one_delete) );
+            result = max(result, max(no_delete, one_delete));
         }
 
-    return result;
+        return result;
     }
 };
