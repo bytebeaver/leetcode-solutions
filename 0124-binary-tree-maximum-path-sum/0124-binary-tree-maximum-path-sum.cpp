889229e53da154843b9f68d9c2e028e23a31d898
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-04 22:53:13 +0530

    Time: 0 ms (100%), Space: 28 MB (45.78%) - LeetHub

--- 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
+++ 0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.cpp
@@ -18,19 +18,14 @@ public:
         
         if(!root) return 0;
 
-        int left_subtree_path_sum = pathSum_Maxi(root->left);
-        int right_subtree_path_sum = pathSum_Maxi(root->right);
+        int left_subtree_path_sum = max(0,pathSum_Maxi(root->left));
+        int right_subtree_path_sum = max (0,pathSum_Maxi(root->right));
 
         int pathsum = left_subtree_path_sum + right_subtree_path_sum + root->val;
 
         result = max(result, pathsum);
 
-        int temp = max(left_subtree_path_sum, right_subtree_path_sum) + root->val;
-
-        if(temp >=0)
         return max(left_subtree_path_sum, right_subtree_path_sum) + root->val;
-
-        else return 0;
     }
     int maxPathSum(TreeNode* root) {
         
