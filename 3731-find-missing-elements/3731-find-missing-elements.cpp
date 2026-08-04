3506670811039042675fcdb10baea8e84d179bcc
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-05 00:16:32 +0530

    Time: 4 ms (35.49%), Space: 33.9 MB (8.08%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,49 +1,42 @@
 class Solution {
 public:
-//     vector<int> findMissingElements(vector<int>& nums) {
-        
-//         int n = nums.size();
-//         sort(nums.begin(), nums.begin() +n);
-//         vector<int> ans;
+    vector<int> approach1_sortAndFillGaps(vector<int>& nums) {
+    int n = nums.size();
+    sort(nums.begin(), nums.begin() + n);   // sort so consecutive pairs make sense
+    vector<int> ans;
 
-//         for( int i=0; i<n-1; i++)
-//         {
-//             for(int val = nums[i] + 1; val < nums[i+1]; val++)
-//             {
-//                 ans.push_back(val);
-//             }
-//         }
-
-//         return ans;
-//     }
-
-
-     vector<int> findMissingElements(vector<int>& nums) {
-        
-       int n = nums.size();
-
-       map<int, bool> m;
-
-        int min_ele = INT_MAX;
-        int max_ele = INT_MIN;
-
-       for(auto & val : nums)
-       {
-            m[val] = true;
-            min_ele =min(min_ele, val);
-            max_ele =max(max_ele, val);
-       }
-
-
-        vector<int> ans;
-
-
-       for(int i=min_ele+1; i<max_ele; i++)
-       {
-            if(m.find(i) == m.end() )
-            ans.push_back(i);
-       }
+    for (int i = 0; i < n - 1; i++)
+    {
+        // fill all numbers strictly between nums[i] and nums[i+1]
+        for (int val = nums[i] + 1; val < nums[i+1]; val++)
+        {
+            ans.push_back(val);   // val is missing since it's not in nums
+        }
+    }
 
     return ans;
+}
+
+     vector<int> findMissingElements(vector<int>& nums) {
+    map<int, bool> m;              // stores which numbers are present
+    int min_ele = INT_MAX;
+    int max_ele = INT_MIN;
+
+    for (auto& val : nums)
+    {
+        m[val] = true;                       // mark val as present
+        min_ele = min(min_ele, val);         // track smallest element
+        max_ele = max(max_ele, val);         // track largest element
     }
+
+    vector<int> ans;
+
+    for (int i = min_ele + 1; i < max_ele; i++)
+    {
+        if (m.find(i) == m.end())            // i not present in map
+            ans.push_back(i);                // so i is missing
+    }
+
+    return ans;
+}
 };
