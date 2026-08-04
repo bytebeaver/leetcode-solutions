ff728f03cb8a939a52ea1eefe6995603adae8c9c
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-04 23:39:16 +0530

    Time: 0 ms (100%), Space: 32.3 MB (54.61%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -8,10 +8,9 @@ public:
 
         for( int i=0; i<n-1; i++)
         {
-            int var = nums[i];
-            for(int j=0; j< nums[i+1]-nums[i]-1; j++)
-            {     var +=1;
-                 ans.push_back(var);
+            for(int val = nums[i] + 1; val < nums[i+1]; val++)
+            {
+                ans.push_back(val);
             }
         }
 
