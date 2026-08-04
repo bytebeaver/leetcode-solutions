78b3f439f16b74a9bc3f45523f11712c8792c3ce
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-05 00:05:28 +0530

    Time: 6 ms (21.92%), Space: 33.8 MB (8.08%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,19 +1,49 @@
 class Solution {
 public:
-    vector<int> findMissingElements(vector<int>& nums) {
+//     vector<int> findMissingElements(vector<int>& nums) {
         
-        int n = nums.size();
-        sort(nums.begin(), nums.begin() +n);
+//         int n = nums.size();
+//         sort(nums.begin(), nums.begin() +n);
+//         vector<int> ans;
+
+//         for( int i=0; i<n-1; i++)
+//         {
+//             for(int val = nums[i] + 1; val < nums[i+1]; val++)
+//             {
+//                 ans.push_back(val);
+//             }
+//         }
+
+//         return ans;
+//     }
+
+
+     vector<int> findMissingElements(vector<int>& nums) {
+        
+       int n = nums.size();
+
+       map<int, bool> m;
+
+        int min_ele = INT_MAX;
+        int max_ele = INT_MIN;
+
+       for(auto & val : nums)
+       {
+            m[val] = true;
+            min_ele =min(min_ele, val);
+            max_ele =max(max_ele, val);
+       }
+
+
         vector<int> ans;
 
-        for( int i=0; i<n-1; i++)
-        {
-            for(int val = nums[i] + 1; val < nums[i+1]; val++)
-            {
-                ans.push_back(val);
-            }
-        }
 
-        return ans;
+       for(int i=min_ele+1; i<max_ele; i++)
+       {
+            if(m.find(i) == m.end() )
+            ans.push_back(i);
+       }
+
+    return ans;
     }
 };
