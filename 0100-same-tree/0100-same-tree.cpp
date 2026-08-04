cc4afa1a5ad19cfd034d6ea3ece4e0d75ddcb605
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-04 23:10:44 +0530

    Time: 0 ms (100%), Space: 12.8 MB (78.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -9,20 +9,28 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+//     bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+//         if(!p || !q) return p==q;
+
+//         if(p->val != q->val) return false;
+
+//         bool v1=isSameTree(p->left, q->left);
+//         bool v2=isSameTree(p->right, q->right);
+
+//         return v1 && v2;
+//     }
+// };
+
+
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         
-        if(!p && !q) return true;
+        if(!p || !q) return p==q;
 
-        if(p && !q) return false;
-        if(!p && q) return false;
-
-        if(p->val != q->val) return false;
-
-        bool v1=isSameTree(p->left, q->left);
-        bool v2=isSameTree(p->right, q->right);
-
-        return v1 && v2;
+        return (p->val == q->val) && isSameTree(p->left, q->left) &&isSameTree(p->right, q->right);
     }
 };
