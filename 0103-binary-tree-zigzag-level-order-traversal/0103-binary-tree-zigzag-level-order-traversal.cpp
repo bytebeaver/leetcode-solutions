bf13d6876bc902f144ed0f35437745787103a23e
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-08 21:30:59 +0530

    Time: 0 ms (100%), Space: 15.2 MB (17.1%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -13,52 +13,43 @@ class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         
-        list<TreeNode *> l;
-        
-        vector<vector<int>> zigzag;
-        int turn =0;
+        list<TreeNode*> l;              // deque-like structure (double ended)
+        vector<vector<int>> zigzag;     // final result
+        int turn = 0;                   // 0 = left->right, 1 = right->left
 
-        if(!root) return zigzag;
+        if (!root) return zigzag;       // empty tree edge case
         l.push_back(root);
 
-        while(!l.empty())
-        {
-            int size = l.size();
+        while (!l.empty()) {
+            int size = l.size();        // number of nodes in current level
             vector<int> level;
-            if(turn == 0)
-            {
-                //front se pop krenge and back se push krenge
-                for(int i=0 ; i<size; i++)
-                {
-                     TreeNode * n = l.front();
-                     l.pop_front();
-                     level.push_back(n->val);
-                     if(n->left) l.push_back(n->left);
-                     if(n->right) l.push_back(n->right);
-                }
 
+            if (turn == 0) {
+                // process left to right: pop front, push children at back
+                for (int i = 0; i < size; i++) {
+                    TreeNode* n = l.front();
+                    l.pop_front();
+                    level.push_back(n->val);
+                    if (n->left)  l.push_back(n->left);
+                    if (n->right) l.push_back(n->right);
+                }
                 zigzag.push_back(level);
-                turn =1;
+                turn = 1;                // flip direction for next level
             }
-
-
-            else if(turn == 1)
-            {
-                //back se pop krenge and front se push krenge pehle right child fir left child
-                for(int i=0 ; i<size; i++)
-                {
-                     TreeNode * n = l.back();
-                     l.pop_back();
-                     level.push_back(n->val);
-                     if(n->right) l.push_front(n->right);
-                     if(n->left) l.push_front(n->left);
+            else {
+                // process right to left: pop back, push children at front (right then left)
+                for (int i = 0; i < size; i++) {
+                    TreeNode* n = l.back();
+                    l.pop_back();
+                    level.push_back(n->val);
+                    if (n->right) l.push_front(n->right);
+                    if (n->left)  l.push_front(n->left);
                 }
-
                 zigzag.push_back(level);
-                turn =0;
+                turn = 0;                // flip direction back
             }
         }
 
-    return zigzag;
+        return zigzag;
     }
 };
