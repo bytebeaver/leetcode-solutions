96d5a16452d8d49d9256f61cd6ccb5057d44e770
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-08 21:27:25 +0530

    Time: 3 ms (69.26%), Space: 27.5 MB (34.28%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,41 +11,42 @@
  */
 class Solution {
 public:
-    unordered_map<int, int> mp;
-    
-    TreeNode* BuildTree(vector<int> &postorder, int post_start, int post_end, vector<int> &inorder, int inorder_start, int inorder_end)
-    {
-        if(post_start>post_end || inorder_start > inorder_end)
-        return nullptr;
+    unordered_map<int, int> mp; // value -> inorder index (fast lookup)
 
+    TreeNode* BuildTree(vector<int> &postorder, int post_start, int post_end,
+                         vector<int> &inorder, int inorder_start, int inorder_end)
+    {
+        // base case: empty range means empty subtree
+        if (post_start > post_end || inorder_start > inorder_end)
+            return nullptr;
+
+        // postorder's last element is always the root
         TreeNode* root = new TreeNode(postorder[post_end]);
 
+        // find root's position in inorder array
         int idx_root = mp[root->val];
 
+        // number of nodes in left subtree
         int left_size = idx_root - inorder_start;
 
-        root->left = BuildTree(postorder, post_start, post_start + left_size -1, inorder, inorder_start , idx_root -1);
+        // build left subtree using left part of postorder & inorder
+        root->left = BuildTree(postorder, post_start, post_start + left_size - 1,
+                                inorder, inorder_start, idx_root - 1);
 
-        root->right = BuildTree(postorder, post_start + left_size , post_end -1 , inorder, idx_root +1 , inorder_end);
+        // build right subtree using right part of postorder & inorder
+        root->right = BuildTree(postorder, post_start + left_size, post_end - 1,
+                                 inorder, idx_root + 1, inorder_end);
 
-
-        return root;
-
+        return root; // return current subtree's root
     }
 
-
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        
-        for(int i=0; i<inorder.size(); i++)
-        mp[inorder[i] ] = i;
+        // pre-fill map: value -> its index in inorder
+        for (int i = 0; i < inorder.size(); i++)
+            mp[inorder[i]] = i;
 
-        int post_start = 0;
-        int post_end = postorder.size()-1;
-
-
-        int inorder_start = 0;
-        int inorder_end = inorder.size()-1;
-
-        return BuildTree(postorder, post_start, post_end, inorder, inorder_start , inorder_end);
+        // call helper with full range of both arrays
+        return BuildTree(postorder, 0, postorder.size() - 1,
+                          inorder, 0, inorder.size() - 1);
     }
 };
