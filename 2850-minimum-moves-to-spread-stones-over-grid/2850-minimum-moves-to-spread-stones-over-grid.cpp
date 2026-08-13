900d57477cb5cb41d027d1cb481be8126a7194ac
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-13 19:37:57 +0530

    Time: 0 ms (100%), Space: 22.8 MB (26.88%) - LeetHub

---
+++ 2850-minimum-moves-to-spread-stones-over-grid/2850-minimum-moves-to-spread-stones-over-grid.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>>xtra, zero;
+
+    int ret;
+    
+    void solve(int i, int count)
+    {
+        if(i>=zero.size()) //all zero stones have been handled
+        {
+            ret = min(ret, count);
+            return ;
+        }
+
+
+        for(int k = 0; k< xtra.size(); k++)
+        {
+            if(xtra[k][2] == 0) continue;
+            xtra[k][2]--;
+
+            solve(i+1, abs(xtra[k][0] - zero[i][0]) + abs(xtra[k][1] - zero[i][1]) + count);
+            xtra[k][2]++;
+
+        }
+    }
+    int minimumMoves(vector<vector<int>>& grid) {
+        
+        for(int i =0 ; i<3 ; i++)
+        {
+            for(int j=0; j<3 ; j++)
+            {
+                if(grid[i][j] == 0)
+                zero.push_back({i,j});
+
+                else if(grid[i][j] > 1)
+                xtra.push_back({i,j, grid[i][j]-1});
+
+            }
+        }
+
+        if(zero.size() == 0)
+        return 0;
+
+        ret = INT_MAX;
+        solve(0,0);
+        return ret;
+    }
+};
