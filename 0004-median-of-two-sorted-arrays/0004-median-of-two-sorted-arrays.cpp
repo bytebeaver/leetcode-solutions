c4bb2e2c40e35dc80be80e7ec49085bd2b5a777a
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-15 12:16:35 +0530

    Time: 0 ms (100%), Space: 95.2 MB (61.64%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,61 +1,59 @@
 class Solution {
- public:
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        int m = nums1.size();
+        int n = nums2.size();
 
- vector<int> merge(vector<int> nums1, vector<int> nums2)
- {
-    int i=0,j=0;
-    int n = nums1.size();
-    int m = nums2.size();
-    vector<int> merged(m+n);
-
-    int idx=0;
-    while(i<n && j<m)
-    {
-        if(nums1[i] <= nums2[j])
-        {
-        merged[idx] = nums1[i];
-        idx++;
-        i++;
+        if(m>n){
+            return findMedianSortedArrays(nums2, nums1);
         }
 
-        else if(nums1[i] > nums2[j])
+        int low = 0;
+        int high = m;
+
+        int left_half = (m+n+1)/2;
+
+        while(low <= high)
         {
-            merged[idx] = nums2[j];
-        idx++;
-        j++;
+            int mid = low + (high - low)/2 ;
+
+            int Px = mid;
+
+            int Py = left_half - Px;
+
+            int x1 = (Px == 0)? INT_MIN : nums1[Px-1];
+            int x2 = (Py == 0)? INT_MIN : nums2[Py-1];
+
+
+            int x3 = (Px == m)? INT_MAX : nums1[Px];
+            int x4 = (Py == n)? INT_MAX : nums2[Py];
+
+            if(x1 <= x4 && x2 <= x3)
+            {
+                //right partition found
+                if( (m+n)%2 == 1) //odd number of elements
+                return max(x1, x2); //left half ka last element
+
+                else //even number of elements
+                {
+                    int t1 = max(x1, x2);
+                    int t2 = min(x3, x4);
+
+                    return (t1 + t2)/2.0; //2.0 because it will convert it will give the exact answer as in double
+                }
+
+            }
+
+            else if( x1 > x4) //we have to go in left of nums 1
+                high = mid-1;
+
+            else 
+                low = mid+1;
+
+
+        
         }
+    return -1;
     }
-
-    while(i<n){
-         merged[idx] = nums1[i];
-        idx++;
-        i++;
-    }
-
-     while(j<m){
-         merged[idx] = nums2[j];
-        idx++;
-        j++;
-    }
-
-    return merged;
- }
-  double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-    
-    vector<int> Merge = merge(nums1 , nums2);
-
-    int size = Merge.size();
-    
-    if(size%2 !=0)
-    {
-        return (double)Merge[size/2];
-    }
-
-    else
-    {
-        return (double)(Merge [size/2] + Merge[size/2 -1])/2;
-    }
-
-    return 0;
-  }
 };
