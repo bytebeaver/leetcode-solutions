97eafb5f9dc380cabbd33f7a4cc28031bbac0274
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-15 19:33:17 +0530

    Time: 4 ms (14.14%), Space: 22.1 MB (42.24%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+      
+      stack<int> st;
+     
+       for( int &a : asteroids)
+       {
+            //when collision happens
+            while(!st.empty() && a< 0 && st.top()> 0)
+            {
+                int sum = a + st.top();
+
+                if(sum<0)
+                st.pop();
+
+                else if(sum > 0)
+                {
+                    a=0;
+                }
+
+                else if(sum ==0)
+                {
+                    st.pop();
+                    a=0;
+                }
+            }
+
+            if(a!=0)
+            st.push(a);
+       }
+
+       int s = st.size();
+
+       vector<int> result(s);
+
+       int i = s-1;
+
+       while(i >=0)
+       {
+        result[i] = st.top();
+        st.pop();
+        i--;
+       }
+
+       return result;
+
+    
+}
+     
+
+};
