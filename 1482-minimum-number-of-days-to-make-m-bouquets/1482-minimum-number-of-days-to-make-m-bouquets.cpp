83b38376c30c674347e783cd4d960729bfc5c073
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-15 17:19:22 +0530

    Time: 27 ms (27.82%), Space: 122.8 MB (6.2%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -60,67 +60,81 @@
 // };
 
 
-
-
 class Solution {
 public:
 
-     bool possible(int waiting_days, vector<int>bloomDay, int m , int k)
-     {
-            //we will check if it possible to make m bouqets if we wait the given number of waiting days
-            int counter = 0;
+    bool possible(int waiting_days, vector<int> bloomDay, int m, int k)
+    {
+        // counter track karta hai consecutive (adjacent) khile hue flowers ki count
+        // adjacency zaroori hai kyunki bouquet ke liye flowers ek saath hone chahiye
+        int counter = 0;
 
-            for(auto & it : bloomDay)
+        for(auto& it : bloomDay)
+        {
+            if(it <= waiting_days)   
+            // <= isliye kyunki flower jo pehle bhi khil chuka hai (bloomDay < waiting_days)
+            // wo bhi ab tak available hai, sirf == wale din khila flower nahi
             {
-                if(it <= waiting_days)
-                {
-                    counter++;
-                    if(counter == k)
-                    {   
-                        counter=0;
-                        m--;
-                        
-                        if(m==0)
-                        return true;
-                    }
-                }
-
-                else if(it > waiting_days)
-                {
-                    counter =0;
+                counter++;
+                if(counter == k)
+                {   
+                    // k consecutive flowers mil gaye -> ek bouquet complete
+                    counter = 0;   // reset karo agla bouquet count karne ke liye
+                    m--;
+                    
+                    if(m == 0)
+                    return true;   
+                    // IMPORTANT FIX: jaise hi required m bouquets ban jaayein, turant true return karo
+                    // isse m negative nahi hoga agar aur bhi extra bouquets ban sakte the
                 }
             }
 
+            else if(it > waiting_days)
+            {
+                // ye flower abhi nahi khila -> adjacency chain toot gayi
+                counter = 0;
+            }
+        }
+
+        // loop poora chal gaya lekin m abhi bhi 0 nahi hua -> is din tak possible nahi
         return false;
-     }
+    }
 
 
     int minDays(vector<int>& bloomDay, int m, int k) {
         
-        int l = 1;//minimum ek din to wait krenge
+        int l = 1;  // minimum ek din to wait karna hi padega (day 0 possible nahi hota bloom ke liye)
 
-        int r = *max_element(begin(bloomDay), end(bloomDay));
+        int r = *max_element(begin(bloomDay), end(bloomDay));  
+        // sabse zyada wait karna padega itna, isse aage koi naya flower khilega hi nahi
 
+        // impossible check: agar total zaroori flowers (m*k) available flowers (n) se zyada hain
+        // to koi din kaam nahi karega -> seedha -1
         if((long long) m*k > bloomDay.size())
         return -1;
 
-        while(l<r)
+        // Binary search on answer: hum "minimum day jispe possible() true ho" dhoond rahe hain
+        while(l < r)
         {
-            int mid = l+ (r-l)/2;
+            int mid = l + (r-l)/2;   // overflow-safe mid calculation
 
             int waiting_days = mid;
 
             if(possible(waiting_days, bloomDay, m, k))
             {
-                r=mid;
+                // agar mid din kaafi hai, to shayad isse kam din mein bhi ho jaaye
+                // isliye mid ko range mein rakhte hue right ko chhota karo
+                r = mid;
             }
 
             else
             {
-                l = mid+1;
+                // mid din kaafi nahi hai, to answer isse zyada hoga
+                l = mid + 1;
             }
         }
 
-    return l;
+        // jab l == r, wahi smallest valid day hai
+        return l;
     }
 };
