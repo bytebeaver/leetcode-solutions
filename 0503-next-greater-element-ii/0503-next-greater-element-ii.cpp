5bcb44e9fe539fde901daf47c9a63801bdbe49df
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-19 21:33:49 +0530

    Time: 5 ms (45.38%), Space: 28.5 MB (40.01%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -7,53 +7,19 @@ public:
         stack<int> st;
         vector<int> nge(n);
 
-        // int last = nums[n-1];
-       int maxi = *max_element(begin(nums), end(nums));
-        // for(int i=0; i<n; i++)
-        // {
-        //     if(nums[i] > last)
-        //     {
-        //         st.push(nums[i]);
-        //         break;
-        //     }
-        // }
+        for(int i = 2*n - 1; i>=0; i--)
+        {
+            while(!st.empty() && st.top() <= nums[i%n])
+            st.pop();
 
-        //will traverse from the back 
-        for(int i=n-1; i>=0; i--)
-        { 
-            if(nums[i] == maxi)
+            if(i<n)
             {
-            nge[i] = -1;
-            st.push(nums[i]);
+                nge[i] = (st.empty())?-1:st.top();
             }
 
-            else
-            {
-                while(  !st.empty() && st.top()<=nums[i] )
-                st.pop();
-
-                if(st.empty())
-                {
-                    for(int j=0; j<i; j++)
-                    {
-                        if(nums[j] > nums[i])
-                        {
-                        nge[i] = nums[j];
-                        break;
-                        }
-
-                    }
-
-                
-                }
-
-                else
-                nge[i]= st.top();
-
-
-                st.push( nums[i] );
-            }
+            st.push(nums[i%n]);
         }
+
     return nge;
     }
 };
