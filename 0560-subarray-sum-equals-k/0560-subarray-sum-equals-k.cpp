4c4435290dba3a1fb3fd62c050ea975334c732e6
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-21 18:56:47 +0530

    Time: 93 ms (22.27%), Space: 53.9 MB (5.28%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,22 +1,51 @@
+// class Solution {
+// public:
+//     int subarraySum(vector<int>& nums, int k) {
+        
+//         int count = 0;
+        
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             int sum=0;
+//             for(int j=i; j<nums.size(); j++)
+//             {
+//                 sum += nums[j];
+
+//                 if(sum == k)
+//                 count++;
+
+//             }
+//         }
+
+//         return count;
+//     }
+// };
+
+
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        
-        int count = 0;
-        
-        for(int i=0; i<nums.size(); i++)
-        {
-            int sum=0;
-            for(int j=i; j<nums.size(); j++)
-            {
-                sum += nums[j];
+       
+       int sum = 0;
+       int count = 0;
 
-                if(sum == k)
-                count++;
+       map<int, int> freq;
+        freq[0] = 1;
 
-            }
-        }
+       for(int i=0; i< nums.size(); i++)
+       {
+          sum += nums[i];
 
-        return count;
+          int partial = sum -k;
+
+          if( freq[partial])
+          count += freq[partial];
+
+          freq[sum]++;
+
+       }
+
+       return count;
+       
     }
 };
