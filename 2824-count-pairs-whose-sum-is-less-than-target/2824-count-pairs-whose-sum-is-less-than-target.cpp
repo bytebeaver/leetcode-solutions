ba094477ed15e62ed2b9837fc6a8aefceb502b0c
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-24 19:53:29 +0530

    Time: 1 ms (14.52%), Space: 24.5 MB (86.96%) - LeetHub

--- 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
+++ 2824-count-pairs-whose-sum-is-less-than-target/2824-count-pairs-whose-sum-is-less-than-target.cpp
@@ -1,25 +1,60 @@
 class Solution {
 public:
     int countPairs(vector<int>& nums, int target) {
-        
+
         int n = nums.size();
 
+        // Sort the array.
+        // This allows us to reason about all elements
+        // between l and r at once.
         sort(nums.begin(), nums.end());
 
-        int l=0;
-        int r= n-1;
+        // l represents the first element of the pair.
+        int l = 0;
 
+        // r represents the second element of the pair.
+        int r = n - 1;
+
+        // Stores the total number of valid pairs.
         int ans = 0;
-        while(l<r)
+
+        while(l < r)
         {
+            // Current pair.
             int sum = nums[l] + nums[r];
 
+            // The current sum is too large or equal to target.
+            //
+            // (l, r) is invalid.
+            //
+            // Since r is the largest element in the current range,
+            // we try a smaller element by moving r left.
             if(sum >= target)
-            r--;
-
-            else if(sum < target)
             {
-                ans += r-l;
+                r--;
+            }
+
+            // The current sum is strictly smaller than target.
+            //
+            // Since the array is sorted:
+            //
+            // nums[l] + nums[l+1]
+            // nums[l] + nums[l+2]
+            // ...
+            // nums[l] + nums[r]
+            //
+            // are all <= nums[l] + nums[r].
+            //
+            // Since nums[l] + nums[r] < target,
+            // ALL of these pairs are valid.
+            //
+            // Number of such pairs = r - l.
+            else
+            {
+                ans += r - l;
+
+                // We have counted every valid pair
+                // involving nums[l], so move l forward.
                 l++;
             }
         }
