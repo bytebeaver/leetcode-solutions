ed9adfa185e34f5147307d703a66eae93e6ced8b
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-25 21:02:28 +0530

    Time: 15 ms (89.21%), Space: 139.2 MB (73.77%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -2,23 +2,23 @@ class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         int i = 0, j = 0;
-        list<int> mylist;      // non-increasing list, front = current max
+        deque<int> q;      // non-increasing list, front = current max
         vector<int> ans;
 
         while (j < nums.size()) {
             // remove strictly smaller elements from back (they're useless now)
-            while (mylist.size() > 0 && mylist.back() < nums[j])
-                mylist.pop_back();
+            while (!q.empty() > 0 && q.back() < nums[j])
+                q.pop_back();
 
-            mylist.push_back(nums[j]);   // insert current element
+            q.push_back(nums[j]);   // insert current element
 
             if (j - i + 1 < k) {
                 j++;                     // window still growing
             } else if (j - i + 1 == k) {
-                ans.push_back(mylist.front());     // record current max
+                ans.push_back(q.front());     // record current max
 
-                if (mylist.front() == nums[i])      // leaving element was the max
-                    mylist.pop_front();
+                if (q.front() == nums[i])      // leaving element was the max
+                    q.pop_front();
 
                 i++; j++;                // slide window forward
             }
