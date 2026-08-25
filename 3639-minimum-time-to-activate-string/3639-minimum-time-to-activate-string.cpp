663a6bfac9311103f6344b113a25aebe2a1db2ef
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-25 21:45:23 +0530

    Time: 114 ms (32.14%), Space: 189.3 MB (36.22%) - LeetHub

---
+++ 3639-minimum-time-to-activate-string/3639-minimum-time-to-activate-string.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+    int minTime(string s, vector<int>& order, int k) {
+        
+        int n = order.size();
+        set<int> star_idx_set;
+        star_idx_set.insert(-1);
+        star_idx_set.insert(n);
+
+        long long count = 0;
+
+        int t=0;
+
+        while(t<n)
+        {
+            int x = order[t];
+
+            star_idx_set.insert(x);
+
+            auto it = star_idx_set.find(x);
+            int star_idx = *it;
+
+            auto prev_it = prev(it);
+            auto next_it = next(it);
+
+            int before = *prev_it;
+            int after = *next_it;
+
+            count += 1LL*(star_idx - before) * (after - star_idx);
+
+            if(count >= k)
+            return t;
+
+            else
+            t++;
+
+        }
+
+        return -1;
+    }
+};
