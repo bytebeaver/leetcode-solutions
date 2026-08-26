c7aa6fee0b42b071bdb0fdf6a706a0d4b397d7dd
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-26 19:38:45 +0530

    Time: 0 ms (100%), Space: 19.1 MB (13.43%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.cpp
@@ -2,29 +2,42 @@ class Solution {
 public:
     bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
         
-        int m = pushed.size();
+        int n = pushed.size();
 
+        // Simulate the actual stack operations.
         stack<int> st;
 
-        int i=0;
-        int j=0;
+        // i -> next element to push
+        // j -> next element that we need to pop
+        int i = 0;
+        int j = 0;
 
-        while(i<m && j<m)
+        while(i < n)
         {
+            // Push the next element according to the
+            // given pushed sequence.
+
             st.push(pushed[i]);
 
-            while(!st.empty() && st.top() == popped[j] && j<m)
+            /*If the top of the stack is the next element
+             required by popped, we can safely pop it.
+             Keep doing this because after popping one element,
+             another element may also become available.*/ 
+
+             
+            while(!st.empty() &&
+                  j < n &&
+                  st.top() == popped[j])
             {
                 st.pop();
                 j++;
             }
 
-        i++;
+            i++;
         }
 
-        if(st.empty())
-        return true;
-
-    return false;
+        // If the stack is empty, every element was successfully
+        // popped in the required order.
+        return st.empty();
     }
 };
