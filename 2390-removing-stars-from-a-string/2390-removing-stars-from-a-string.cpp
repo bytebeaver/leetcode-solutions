7f011ebf7e3593853b2ce578c3884179ebdc3dc0
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-26 18:54:32 +0530

    Time: 27 ms (18.88%), Space: 28.4 MB (64.31%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -1,47 +1,76 @@
+// class Solution {
+// public:
+//     string removeStars(string s) {
+        
+//         int n = s.length();
+
+//         stack<char>st;
+
+//         int i= 0;
+//         while(i<n)
+//         {
+//             if(!st.empty() && s[i] =='*')
+//             {
+//                 st.pop();
+//                 i++;
+//             }
+
+//             else
+//             {
+//                 st.push(s[i]);
+//                 i++;
+//             }
+//         }
+
+//         int stack_size = st.size();
+
+//         // string str(stack_size, ' ');
+
+//         // for(int i=stack_size-1; i>=0; i--)
+//         // {
+//         //     str[i]= st.top();
+//         //     st.pop();
+//         // }
+
+//         string str = "";
+
+//         while(!st.empty())
+//         {
+//             str.push_back( st.top() );
+//             st.pop();
+//         }
+
+//         reverse(begin(str), end(str));
+
+//     return str;
+//     }
+// };
+
 class Solution {
 public:
     string removeStars(string s) {
         
         int n = s.length();
 
-        stack<char>st;
-
+        string result ="";
         int i= 0;
         while(i<n)
         {
-            if(!st.empty() && s[i] =='*')
+            if(s[i] =='*')
             {
-                st.pop();
+                result.pop_back();
                 i++;
             }
 
             else
             {
-                st.push(s[i]);
+                result.push_back(s[i]);
                 i++;
             }
         }
 
-        int stack_size = st.size();
 
-        // string str(stack_size, ' ');
-
-        // for(int i=stack_size-1; i>=0; i--)
-        // {
-        //     str[i]= st.top();
-        //     st.pop();
-        // }
-
-        string str = "";
-
-        while(!st.empty())
-        {
-            str.push_back( st.top() );
-            st.pop();
-        }
-
-        reverse(begin(str), end(str));
-
-    return str;
+    return result;
     }
 };
+
