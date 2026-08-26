91b2054ad2c6a2892c1555f0462c9f922432fa8e
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-26 18:58:21 +0530

    Time: 29 ms (13.82%), Space: 28.5 MB (52.54%) - LeetHub

--- 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.cpp
@@ -4,12 +4,23 @@
         
 //         int n = s.length();
 
-//         stack<char>st;
+//         // We use a stack because:
+//         // Whenever we encounter '*', we need to remove
+//         // the character immediately before it.
+//         // The most recently added character is exactly
+//         // what a stack gives us through pop().
+//         stack<char> st;
 
-//         int i= 0;
-//         while(i<n)
+//         int i = 0;
+
+//         while(i < n)
 //         {
-//             if(!st.empty() && s[i] =='*')
+//             // If current character is '*', remove the
+//             // most recently added character.
+//             //
+//             // We check !st.empty() so that we don't try
+//             // to pop from an empty stack.
+//             if(!st.empty() && s[i] == '*')
 //             {
 //                 st.pop();
 //                 i++;
@@ -17,60 +28,75 @@
 
 //             else
 //             {
+//                 // Normal character:
+//                 // Add it to the stack because it may
+//                 // need to be removed by a future '*'.
 //                 st.push(s[i]);
 //                 i++;
 //             }
 //         }
 
-//         int stack_size = st.size();
-
-//         // string str(stack_size, ' ');
-
-//         // for(int i=stack_size-1; i>=0; i--)
-//         // {
-//         //     str[i]= st.top();
-//         //     st.pop();
-//         // }
-
+//         // The stack contains the remaining characters,
+//         // but in reverse order because stack follows LIFO.
 //         string str = "";
 
 //         while(!st.empty())
 //         {
-//             str.push_back( st.top() );
+//             // Take the top character and add it to the string.
+//             // Since we are removing from the top, the string
+//             // will initially be constructed backwards.
+//             str.push_back(st.top());
 //             st.pop();
 //         }
 
+//         // Reverse to restore the original left-to-right order.
 //         reverse(begin(str), end(str));
 
-//     return str;
+//         return str;
 //     }
 // };
 
+
+// approach 2 ---in cpp string itself work as stack and has push_back() and pop_back()
 class Solution {
 public:
     string removeStars(string s) {
         
         int n = s.length();
 
-        string result ="";
-        int i= 0;
-        while(i<n)
+        // We use 'result' as a stack.
+        //
+        // push_back() adds a character to the end.
+        // pop_back() removes the last character.
+        string result = "";
+
+        int i = 0;
+
+        while(i < n)
         {
-            if(s[i] =='*')
+            if(s[i] == '*')
             {
+                // '*' means:
+                // Remove the character immediately before it.
+                //
+                // Since result stores all previous characters,
+                // the last character is exactly the one we need
+                // to remove.
                 result.pop_back();
+
                 i++;
             }
 
             else
             {
+                // Normal character:
+                // Add it to result.
                 result.push_back(s[i]);
+
                 i++;
             }
         }
 
-
-    return result;
+        return result;
     }
 };
-
