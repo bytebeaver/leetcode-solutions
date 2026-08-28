ee05071fab938861fc79e4c1ca3655b52d26644e
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-28 12:47:31 +0530

    Time: 124 ms (14.5%), Space: 99.8 MB (15.72%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -4,44 +4,41 @@ public:
         
 
         int n = nums.size();
-        map<int, int> mp;
+        unordered_set<int> st;
 
-        long long sum =0;
-        // mp[ nums[0] ] = 0;
+        long long current_window_sum =0;
+       
         int l=0, r=0;
         
         long long max_sum =0;
 
         while(r<n)
         {
-            sum += nums[r];
-
-            if(mp.find(nums[r]) != mp.end() && mp[nums[r]] >= l)
+            
+            while(st.count( nums[r] ))
             {
-                while(l!= mp[nums[r]] + 1)
-                {
-                    sum-=nums[l];
-                    l++;
-                }
+                current_window_sum -= nums[l];  
+                st.erase( nums[l] );
+                l++; 
             }
 
-            if( r-l+1 < k)
-            {
-                mp[nums[r]] = r;
-                r++;
-            }
+            current_window_sum += nums[r];
+            st.insert( nums[r] );
+
+            if(r-l+1 < k)
+            r++;
 
             else if(r-l+1 == k)
             {
-                mp[nums[r]] = r;
-
-                max_sum = max(max_sum , sum);
-                sum -= nums[l];
+                max_sum = max(max_sum, current_window_sum);
+                current_window_sum -= nums[l];
+                st.erase( nums[l] );
                 l++;
                 r++;
-            }
 
+            }
         }
+          
 
     return max_sum;
     }
