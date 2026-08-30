411f7b90408f61f6cc141297354d8c6f1d32117a
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-08-31 04:30:37 +0530

    Time: 0 ms (100%), Space: 46.2 MB (94.86%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,27 +1,44 @@
 class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
-        
+
         int n = cardPoints.size();
 
+        // Sum of cards currently taken from the left side
         int left_sum = 0;
+
+        // Sum of cards currently taken from the right side
         int right_sum = 0;
 
-        for(int i=0; i<=k-1; i++)
-        left_sum += cardPoints[i];
+        // Initially, take all k cards from the left
+        for (int i = 0; i <= k - 1; i++)
+            left_sum += cardPoints[i];
 
+        // Initially, our score is the sum of first k cards
         int max_points = left_sum;
 
-        int right_index=n-1;
+        // Start taking cards from the rightmost position
+        int right_index = n - 1;
 
-        for(int left_index = k-1; left_index>=0; left_index--)
+        // Gradually replace left cards with right cards
+        for (int left_index = k - 1;
+             left_index >= 0;
+             left_index--)
         {
+            // Remove one card from the left selection
             left_sum -= cardPoints[left_index];
+
+            // Add one card from the right selection
             right_sum += cardPoints[right_index];
 
-            max_points = max(max_points, left_sum + right_sum);
+            // Calculate the score for this combination
+            int current_score = left_sum + right_sum;
 
-            right_index -= 1;
+            // Update maximum score
+            max_points = max(max_points, current_score);
+
+            // Move to the next card from the right
+            right_index--;
         }
 
         return max_points;
