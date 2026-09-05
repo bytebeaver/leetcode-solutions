ecf400ca289b9802c5076a542de4ef11ecc6f04c
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-09-05 14:13:05 +0530

    Time: 31 ms (73.98%), Space: 50.7 MB (74.91%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,51 +1,136 @@
+// class LRUCache {
+// public:
+//     vector< pair<int, int>> cache;
+//     int n;
+//     LRUCache(int capacity) {
+//          n = capacity;
+//     }
+    
+//     int get(int key) {
+        
+//         for(int i=0; i<cache.size() ; i++)
+//         {
+//             if( cache[i].first == key)
+//             {
+//                 int val = cache[i].second;
+
+//                 pair<int, int> temp = cache[i];
+//                 cache.erase( cache.begin() + i);
+//                 cache.push_back(temp );
+
+//                 return val;
+//             }
+//         }
+//         return -1;
+//     }
+    
+//     void put(int key, int value) {
+        
+//          for(int i=0; i<cache.size() ; i++)
+//          {
+//             if( cache[i].first == key)
+//             {
+//                 cache[i].second = value;
+//                 pair<int, int> temp = cache[i];
+//                 cache.erase( cache.begin() + i);
+//                 cache.push_back(temp );
+
+//                 return ;
+//             }
+//          }
+
+//          if( cache.size() == n)
+//          {
+//             //get rid of least recently used
+//               cache.erase( cache.begin());
+//          }
+
+//          cache.push_back( {key, value});
+//      }
+// };
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+
+
+
 class LRUCache {
 public:
-    vector< pair<int, int>> cache;
-    int n;
+    
+    list<int> dll; //key
+
+    map<int, pair< list<int> :: iterator, int>>mp; // list<int> :: iterator gives address of the node in Doubly linked list;
+
+
+    int n ;
+
     LRUCache(int capacity) {
          n = capacity;
+    }
+
+    void makeRecentlyUsed( int key )
+    {
+        //pehle to ham erse krenge  ....is key ka address mujhe map se milega
+        dll.erase( mp[key].first );
+        
+        //now make it most recently used bu adding it to front
+        dll.push_front(key);
+
+        mp[key].first = dll.begin(); //since the key's position is change to front of the list its address must also be changed and we have to update that in map
     }
     
     int get(int key) {
         
-        for(int i=0; i<cache.size() ; i++)
-        {
-            if( cache[i].first == key)
-            {
-                int val = cache[i].second;
+       if( mp.find(key) ==mp.end()) //if that key does not exist in our cache
+       return -1;
 
-                pair<int, int> temp = cache[i];
-                cache.erase( cache.begin() + i);
-                cache.push_back(temp );
+       //else if it is presnt ..i need to move it to front
+       makeRecentlyUsed(key);
 
-                return val;
-            }
-        }
-        return -1;
+       return mp[key].second; //i have to return the value and that is in map
     }
     
     void put(int key, int value) {
         
-         for(int i=0; i<cache.size() ; i++)
-         {
-            if( cache[i].first == key)
-            {
-                cache[i].second = value;
-                pair<int, int> temp = cache[i];
-                cache.erase( cache.begin() + i);
-                cache.push_back(temp );
+        if(mp.find(key)  != mp.end())
+        {
+            //upate its value and make it most recently used
+            mp[key].second = value;
 
-                return ;
-            }
-         }
+            makeRecentlyUsed( key );
 
-         if( cache.size() == n)
-         {
-            //get rid of least recently used
-              cache.erase( cache.begin());
-         }
+        }
 
-         cache.push_back( {key, value});
+        //if it is not already presnt we have to insert it 
+        else
+        {
+            dll.push_front(key);
+
+            mp[key] = { dll.begin(), value}; //since it is newly inserted at beginning ....its address is given by dll.begin() 
+
+            n--;
+        }
+
+        if( n< 0)// we have more nodes than the allowed capacity
+        {
+            //erase the least recently used and that node will be ata back of list
+            int key_to_be_deleted = dll.back();
+
+            //deleting from map firstly
+            mp.erase( key_to_be_deleted);
+
+            //deleting from list
+            dll.pop_back();
+
+            //since we have deleted we have to update the capacity
+            n++;
+
+        }
      }
 };
 
