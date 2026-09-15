cef95cc0708c74e7e340bc9293deaf4bce8c2576
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-09-15 18:36:23 +0530

    Time: 2 ms (18.34%), Space: 27.1 MB (34.96%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,50 +1,90 @@
+// class Solution {
+// public:
+//     int trap(vector<int>& arr) {
+        
+//         int n = arr.size();
+
+//         int l_max = 0;   // left side ka ab tak ka max height (left wall)
+//         int r_max = 0;   // right side ka ab tak ka max height (right wall)
+
+//         int l = 0;       // left pointer, array ke start se
+//         int r = n - 1;    // right pointer, array ke end se
+
+//         int water = 0;   // total trapped water accumulate karega
+
+//         while (l <= r)   // jab tak dono pointers cross na ho jayein
+//         {
+//             if (l_max <= r_max)
+//             {
+//                 // left side "weaker" hai, isliye left index ka answer
+//                 // ab finalize kiya ja sakta hai (right mein utna hi ya usse bada wall hai)
+//                 if (l_max > arr[l])
+//                 {
+//                     // current bar se lambi wall left mein pehle dekh chuke hain
+//                     // → is bar par paani ruk sakta hai
+//                     water += l_max - arr[l];
+//                 }
+//                 else
+//                 {
+//                     // current bar hi ab tak ki sabse badi left wall hai
+//                     l_max = arr[l];
+//                 }
+//                 l++;   // left pointer aage badhao
+//             }
+
+//             else if (l_max > r_max)
+//             {
+//                 // right side "weaker" hai, similarly process karo
+//                 if (r_max > arr[r])
+//                 {
+//                     water += r_max - arr[r];
+//                 }
+//                 else
+//                 {
+//                     r_max = arr[r];
+//                 }
+//                 r--;   // right pointer peeche laao
+//             }
+//         }
+
+//         return water;
+//     }
+// };
+
+
+
 class Solution {
 public:
     int trap(vector<int>& arr) {
         
         int n = arr.size();
 
-        int l_max = 0;   // left side ka ab tak ka max height (left wall)
-        int r_max = 0;   // right side ka ab tak ka max height (right wall)
+        int water  = 0;
+        stack<int> st;
 
-        int l = 0;       // left pointer, array ke start se
-        int r = n - 1;    // right pointer, array ke end se
-
-        int water = 0;   // total trapped water accumulate karega
-
-        while (l <= r)   // jab tak dono pointers cross na ho jayein
+        for( int i=0; i<n ; i++)
         {
-            if (l_max <= r_max)
+            while( !st.empty() && arr[st.top()] < arr[i])
             {
-                // left side "weaker" hai, isliye left index ka answer
-                // ab finalize kiya ja sakta hai (right mein utna hi ya usse bada wall hai)
-                if (l_max > arr[l])
-                {
-                    // current bar se lambi wall left mein pehle dekh chuke hain
-                    // → is bar par paani ruk sakta hai
-                    water += l_max - arr[l];
-                }
-                else
-                {
-                    // current bar hi ab tak ki sabse badi left wall hai
-                    l_max = arr[l];
-                }
-                l++;   // left pointer aage badhao
+                int bottom = st.top();
+                st.pop();
+
+
+                if( st.empty())
+                break;
+
+                int left = st.top();
+                int right = i;
+
+                int width = right - left - 1;
+
+                int height = min( arr[left], arr[right]) - arr[bottom];
+
+                water += width * height; 
             }
 
-            else if (l_max > r_max)
-            {
-                // right side "weaker" hai, similarly process karo
-                if (r_max > arr[r])
-                {
-                    water += r_max - arr[r];
-                }
-                else
-                {
-                    r_max = arr[r];
-                }
-                r--;   // right pointer peeche laao
-            }
+            st.push( i );
+
         }
 
         return water;
