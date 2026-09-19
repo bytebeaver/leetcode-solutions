35eef60218ff470ef802721b40bf095a3fc98157
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-09-19 12:05:40 +0530

    Time: 4 ms (34.44%), Space: 18 MB (53.25%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    typedef pair<int, int> P;
+
+    struct lambda{
+        bool operator()( P &p1, P &p2)
+        {
+            return p1.second < p2.second;
+        }
+    };
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+        priority_queue< P, vector<P> , lambda> pq;
+
+        map<int, int> mp;
+
+        for( auto &it: nums)
+        {
+            mp[it]++;
+        }
+
+        for( auto &it : mp)
+        {
+            pq.push( {it.first, it.second});
+        }
+
+        vector<int> result;
+        while(!pq.empty() && k>0)
+        {
+            P temp = pq.top();
+            pq.pop();
+
+            result.push_back( temp.first);
+            k--;
+        }
+
+        return result;
+    }
+};
