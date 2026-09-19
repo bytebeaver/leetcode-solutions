6574ab8f732c9d81a87322294c3b0f6064556a41
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-09-19 12:47:32 +0530

    Time: 4 ms (34.44%), Space: 20 MB (17.95%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,21 +1,64 @@
+// class Solution {
+// public:
+
+//     typedef pair<int, int> P;
+
+//     // struct lambda{
+//     //     bool operator()( P &p1, P &p2)
+//     //     {
+//     //         return p1.second > p2.second; //maintaining a min heap
+//     //     }
+//     // };
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+        
+//         // priority_queue< P, vector<P> , lambda> pq; //min heap --- here i have implemented the comparator myself for min heap
+
+//         priority_queue< P, vector<P> , greater<P>> pq; //min heap  ---- greater<P> is inbuilt comparoator for min heap
+ 
+
+//         unordered_map<int, int> mp;
+
+//         for( auto &it: nums)
+//         {
+//             mp[it]++;
+//         }
+
+
+//         //nlogk ---- pushing all in the heap
+//         for( auto &it : mp)  
+//         {
+//             int value = it.first;
+//             int freq =  it.second;
+
+//             pq.push( {freq, value}); //log k
+
+//              if( pq.size() > k) //min heap ka size atmost k rahega ..jese hi k se bada pop teh minimu from top
+//             {
+//                 pq.pop();
+//             }
+//         }
+
+//         vector<int> result;
+//         while(!pq.empty() && k>0)
+//         {
+//             P temp = pq.top();
+//             pq.pop();
+
+//             result.push_back( temp.second); //temp.second has value
+//             k--;
+//         }
+
+//         return result;
+//     }
+// };
+
+
 class Solution {
 public:
 
-    typedef pair<int, int> P;
-
-    // struct lambda{
-    //     bool operator()( P &p1, P &p2)
-    //     {
-    //         return p1.second > p2.second; //maintaining a min heap
-    //     }
-    // };
+ 
     vector<int> topKFrequent(vector<int>& nums, int k) {
         
-        // priority_queue< P, vector<P> , lambda> pq; //min heap --- here i have implemented the comparator myself for min heap
-
-        priority_queue< P, vector<P> , greater<P>> pq; //min heap  ---- greater<P> is inbuilt comparoator for min heap
- 
-
         unordered_map<int, int> mp;
 
         for( auto &it: nums)
@@ -23,30 +66,37 @@ public:
             mp[it]++;
         }
 
+        int n = nums.size();
+        vector<vector<int>> bucket(n+1);
 
-        //nlogk ---- pushing all in the heap
-        for( auto &it : mp)  
+        for( auto &it: mp)
         {
-            int value = it.first;
-            int freq =  it.second;
-
-            pq.push( {freq, value}); //log k
-
-             if( pq.size() > k) //min heap ka size atmost k rahega ..jese hi k se bada pop teh minimu from top
-            {
-                pq.pop();
-            }
+            bucket[ it.second].push_back( it.first);
         }
 
+        int i = bucket.size()-1 ; //bucket .size  is n + 1
+        
         vector<int> result;
-        while(!pq.empty() && k>0)
-        {
-            P temp = pq.top();
-            pq.pop();
+        while( i>=0)
+       {
+            if( bucket[i].size() == 0 )
+            {
+                i--;
+                continue;
+            }
 
-            result.push_back( temp.second);
-            k--;
-        }
+            else
+            {
+                while( bucket[i].size() > 0 && k > 0)
+                {
+                    result.push_back( bucket[i].back());
+                    bucket[i].pop_back();
+                    k--;
+                }
+            }
+
+            i--;
+       }
 
         return result;
     }
