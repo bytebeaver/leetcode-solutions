2e6346bfbd8709f257ebd816359f5bef3ca22d03
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-09-19 12:59:26 +0530

    Time: 3 ms (51.47%), Space: 20.2 MB (5.8%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -52,34 +52,71 @@
 //     }
 // };
 
-
 class Solution {
 public:
 
- 
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
+
+        // Store the frequency of every distinct number.
+        //
+        // Example:
+        // nums = [1,1,1,2,2,3]
+        //
+        // mp = {
+        //     1 : 3,
+        //     2 : 2,
+        //     3 : 1
+        // }
         unordered_map<int, int> mp;
 
-        for( auto &it: nums)
+        for (auto &it: nums)
         {
             mp[it]++;
         }
 
         int n = nums.size();
+
+        // Create n + 1 buckets.
+        //
+        // The bucket index represents the frequency.
+        //
+        // bucket[1] contains numbers occurring once.
+        // bucket[2] contains numbers occurring twice.
+        // bucket[3] contains numbers occurring three times.
+        //
+        // Frequency can range from 1 to n.
         vector<vector<int>> bucket(n+1);
 
-        for( auto &it: mp)
+        // Place each number into its corresponding
+        // frequency bucket.
+        //
+        // it.second = frequency
+        // it.first  = number
+        //
+        // Example:
+        // If number = 1 and frequency = 3:
+        // bucket[3].push_back(1);
+        for (auto &it: mp)
         {
-            bucket[ it.second].push_back( it.first);
+            bucket[it.second].push_back(it.first);
         }
 
-        int i = bucket.size()-1 ; //bucket .size  is n + 1
-        
+        // Start from the last bucket because it represents
+        // the highest possible frequency.
+        //
+        // bucket.size() = n + 1
+        // Last valid index = n
+        int i = bucket.size()-1;
+
         vector<int> result;
-        while( i>=0)
-       {
-            if( bucket[i].size() == 0 )
+
+        // Traverse the buckets from highest frequency
+        // to lowest frequency.
+        while (i >= 0)
+        {
+            // If the current bucket is empty,
+            // move to the next lower frequency.
+            if (bucket[i].size() == 0)
             {
                 i--;
                 continue;
@@ -87,16 +124,30 @@ public:
 
             else
             {
-                while( bucket[i].size() > 0 && k > 0)
+                // Process all numbers in the current bucket.
+                //
+                // back() accesses the last element in O(1).
+                // pop_back() removes the last element in O(1).
+                //
+                // We stop when the bucket becomes empty
+                // or when we have collected k elements.
+                while (bucket[i].size() > 0 && k > 0)
                 {
-                    result.push_back( bucket[i].back());
+                    // Take a number from the current frequency bucket.
+                    result.push_back(bucket[i].back());
+
+                    // Remove the selected number from the bucket.
+                    // Removing from the back takes O(1) time.
                     bucket[i].pop_back();
+
+                    // One required element has been collected.
                     k--;
                 }
             }
 
+            // Move to the next lower frequency bucket.
             i--;
-       }
+        }
 
         return result;
     }
