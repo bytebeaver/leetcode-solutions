983df6942abd4014fd2f9e30be2128851903e72c
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-09-23 19:25:17 +0530

    Time: 68 ms (83.66%), Space: 125.3 MB (82.53%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -4,46 +4,49 @@ public:
         
         int n = profits.size();
         
+        // Store each project as {required capital, profit}.
+        // This allows us to sort projects based on the capital required.
         vector<pair<int, int>> vec(n);
 
-        for( int i=0; i<n ; i++)
+        for(int i = 0; i < n; i++)
         {
-            vec[i] =  {capital[i], profits[i]} ;
+            vec[i] = {capital[i], profits[i]};
         }
 
+        // Sort projects in increasing order of required capital.
+        // This helps us efficiently find all projects that become affordable
+        // as our available capital 'w' increases.
         sort(vec.begin(), vec.end());
 
-        int i=0; 
+        int i = 0;
 
-        priority_queue< int> maxProfit;
-        while( k>0 )
+        // Max-heap containing profits of all currently affordable projects.
+        // The project with the highest profit will always be at the top.
+        priority_queue<int> maxProfit;
+
+        while(k > 0)
         {
-            while( i<n && vec[i].first <= w)
+            // Add every project whose required capital is <= current capital.
+            // Since vec is sorted by capital, we only move 'i' forward.
+            while(i < n && vec[i].first <= w)
             {
                 maxProfit.push(vec[i].second);
                 i++;
             }
 
-            if( maxProfit.empty()) break;
+            // If no project is affordable, we cannot proceed further.
+            if(maxProfit.empty())
+                break;
 
-            if( !maxProfit.empty())
-            {
-                w += maxProfit.top();
-                maxProfit.pop();
-                k--;
-            }
-        }
-
-
-        while( !maxProfit.empty() && k > 0)
-        {
+            // Choose the currently affordable project with maximum profit.
+            // Completing it increases our available capital.
             w += maxProfit.top();
             maxProfit.pop();
-            k--;
 
-            if( k == 0)
-            break;
+            k--;
         }
+
+       
 
         return w;
     }
