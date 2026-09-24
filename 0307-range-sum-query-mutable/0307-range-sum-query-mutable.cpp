156f2b97cf8c6e6a4bcaddadb1bf855751425e48
Yashik <147544198+bytebeaver@users.noreply.github.com>
2026-09-24 20:01:10 +0530

    Time: 49 ms (76.85%), Space: 182.3 MB (51.42%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.cpp
@@ -4,82 +4,109 @@ public:
     vector<int> segTree;
     int n;
 
-    void build( int node, int l , int r, vector<int>&nums)
+    // Build the Segment Tree
+    void build(int node, int l, int r, vector<int>& nums)
     {
-        if( l==r)
+        // Leaf node: store the array element
+        if(l == r)
         {
             segTree[node] = nums[l];
             return;
         }
 
-        int mid = l + (r-l)/2;
+        int mid = l + (r - l) / 2;
 
-        build( 2*node, l , mid, nums);
-        build( 2*node+1, mid+1 , r, nums);
+        // Build left and right subtrees
+        build(2 * node, l, mid, nums);
+        build(2 * node + 1, mid + 1, r, nums);
 
-        segTree[node] = segTree[2*node] + segTree[2*node+1];
-
+        // Store sum of both children
+        segTree[node] =
+            segTree[2 * node] + segTree[2 * node + 1];
     }
 
-    void  updateTree( int node, int l, int r , int idx , int val)
+
+    // Update one element: nums[idx] = val
+    void updateTree(int node, int l, int r, int idx, int val)
     {
-        if( l == r)
+        // Reached the element to update
+        if(l == r)
         {
             segTree[node] = val;
             return;
         }
 
-        int mid = l + (r-l)/2;
+        int mid = l + (r - l) / 2;
 
-        if( idx <= mid)
+        // Move to the child containing idx
+        if(idx <= mid)
         {
-            updateTree( 2*node , l, mid , idx, val);
+            updateTree(2 * node, l, mid, idx, val);
         }
         else
         {
-            updateTree(2*node + 1, mid+1, r , idx , val);
+            updateTree(2 * node + 1, mid + 1, r, idx, val);
         }
 
-         segTree[node] = segTree[2*node] + segTree[2*node+1];
+        // Recalculate sum after updating the child
+        segTree[node] =
+            segTree[2 * node] + segTree[2 * node + 1];
     }
 
+
+    // Return sum of elements in [start, end]
     int queryTree(int node, int start, int end, int l, int r)
     {
-        if( r< start || l > end)
-        return 0;
+        // No overlap
+        if(r < start || l > end)
+            return 0;
 
-        if( l>= start && r<= end)
+        // Complete overlap: use stored sum
+        if(l >= start && r <= end)
         {
             return segTree[node];
         }
 
-        int mid = l + (r-l)/2;
+        int mid = l + (r - l) / 2;
 
-        return queryTree( 2*node, start, end, l , mid) + queryTree(2*node + 1, start, end, mid+1, r);
+        // Partial overlap: query both children
+        return queryTree(2 * node, start, end, l, mid)
+             + queryTree(2 * node + 1, start, end, mid + 1, r);
     }
 
-    NumArray(vector<int>& nums) {
+
+    // Constructor: build the Segment Tree
+    NumArray(vector<int>& nums)
+    {
         n = nums.size();
-        segTree.resize(4*n);
+        segTree.resize(4 * n);
 
-        build( 1, 0, n-1, nums);
+        // Root = 1, range = [0, n-1]
+        build(1, 0, n - 1, nums);
     }
     
-    void update(int index, int val) {
-        updateTree( 1, 0, n-1, index , val); //1 -- root index in segTre array |||| 0 , n-1 -- initial l, r
+
+    // Public function for point update
+    void update(int index, int val)
+    {
+        // 1 = root of Segment Tree
+        // [0, n-1] = complete array range
+        updateTree(1, 0, n - 1, index, val);
     }
     
-    int sumRange(int left, int right) {
-        int start = left;
-        int end  = right;
 
-        return queryTree( 1, start, end , 0 , n-1);
+    // Public function for range sum query
+    int sumRange(int left, int right)
+    {
+        return queryTree(1, left, right, 0, n - 1);
     }
 };
 
+
 /**
  * Your NumArray object will be instantiated and called as such:
+ *
  * NumArray* obj = new NumArray(nums);
- * obj->update(index,val);
- * int param_2 = obj->sumRange(left,right);
+ * obj->update(index, val);
+ * int param_2 = obj->sumRange(left, right);
  */
